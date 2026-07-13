c2802ce2e676fc27e73d54fa1a014ef4e64f1127
ADUI <2500114656@vanlangsaigon.edu.vn>
2026-07-14 02:05:14 +0700

    Move source code into Shoppingonline folder

---
+++ Shoppingonline/client-admin/src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">ADMIN HOME</h2>
+        <img src="https://via.placeholder.com/800x600.png?text=Admin+Home" width="800px" height="600px" alt="" />
+      </div>
+    );
+  }
+}
+export default Home;
